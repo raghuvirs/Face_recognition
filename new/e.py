b920bd00a3b226ba1dfc6ddb881bf0be5c239d12
Raghuvir Singh <107181721+raghuvirs@users.noreply.github.com>
2022-10-07 15:51:19 +0530

    Delete e.py

--- new/e.py
+++
@@ -1,6 +0,0 @@
-import cv2 as cv
-img = cv.imread('12.jpg')
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-har_cascade = cv.CascadeClassifier('harr_face.xml')
-harcascade = har_cascade.dete
-cv.waitKey(0)
